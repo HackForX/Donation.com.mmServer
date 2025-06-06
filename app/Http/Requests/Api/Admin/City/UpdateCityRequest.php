be0d0b4158b4e7fc2d4e7af1e866a13ba8093e93
Ran-13 <linnayye557@gmail.com>
2025-06-06 17:21:53 +0630

    category and city error fixed

--- app/Http/Requests/Api/Admin/City/UpdateCityRequest.php
+++ app/Http/Requests/Api/Admin/City/UpdateCityRequest.php
@@ -24,7 +24,7 @@ class UpdateCityRequest extends FormRequest
         return [
             'name' => [
                 'string',
-                'unique:cities,name'
+              
             ],
 
         ];
